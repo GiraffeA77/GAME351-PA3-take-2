e15000405f1495467e56ccf8c05219c0bdf3e9a8
GiraffeA77 <mrust1@arizona.edu>
2025-03-10 22:01:17 -0600

    final touches

--- Assets/Scripts/CameraChange.cs
+++ Assets/Scripts/CameraChange.cs
@@ -2,34 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class CameraChange : MonoBehaviour {
     public GameObject FirstCam;
     public GameObject ThirdCam;
-    public int CamMode;
+    private bool isFirstPerson = true;
 
-    void update(){
-        if(input.GetButtonDown ("Camera")) {
-            if (CamMode == 1) {
-                CamMode == 0;
-        } else {
-            CamMode += 1;
-        }
-        StartCoroutine (CamChange());
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.T)) {
+            isFirstPerson = !isFirstPerson;
+            if (isFirstPerson) {
+                ActivateFirstPersonCamera();
+            } else {
+                ActivateThirdPersonCamera();
+            }
         }
 
-        IEnumerator CamChange(){
-            yield return new WaitForSeconds(0.01f);
-            if (CamMode == 0) {
-                FirstCam.SetActive(false);
-                ThirdCam.SetActive(true);
-            }
-            if (CamMode == 1){
-                FirstCam.SetActive(true);
-                ThirdCam.SetActive(false);
-            }
+        if (Input.GetKeyDown(KeyCode.R)) {
+            isFirstPerson = true;
+            ActivateFirstPersonCamera();
         }
     }
+
+    void ActivateFirstPersonCamera() {
+        FirstCam.SetActive(true);
+        ThirdCam.SetActive(false);
+    }
+
+    void ActivateThirdPersonCamera() {
+        FirstCam.SetActive(false);
+        ThirdCam.SetActive(true);
+    }
 }
-
-
--- Assets/Scripts/EscapeCutScene.cs
+++ Assets/Scripts/EscapeCutScene.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Playables;
+using Cinemachine;
 
-public class EscapeCutScene : MonoBehaviour
-{
-    PlayableDirector director;
-    // Start is called before the first frame update
-    void Start()
-    {
-        director=GetComponent<PlayableDirector>();
+public class EscapeCutScene : MonoBehaviour {
+    public CinemachineVirtualCamera vcam1;
+    public CinemachineVirtualCamera vcam2;
+    public CinemachineVirtualCamera vcam3;
+    public GameObject gameplayCam;
+    private bool isCutsceneActive = true;
+
+    void Start() {
+        gameplayCam.SetActive(false);
+        vcam1.Priority = 20;
+        vcam2.Priority = 20;
+        vcam3.Priority = 20;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            director.Play();
+    void Update() {
+        if (isCutsceneActive && Input.GetKeyDown(KeyCode.Escape)) {
+            ExitCutscene();
         }
+    }
+
+    void ExitCutscene() {
+        vcam1.Priority = 0;
+        vcam2.Priority = 0;
+        vcam3.Priority = 0;
+        gameplayCam.SetActive(true);
+        isCutsceneActive = false;
     }
 }
